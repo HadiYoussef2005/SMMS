e18c1d996941f95ca60c7c8923d43568a2f7f317
Hadi Youssef <hmyoussef@gmail.com>
2025-05-11 16:44:50 -0400

    fixed db export

--- Backend/db/mongoClient.js
+++ Backend/db/mongoClient.js
@@ -4,11 +4,9 @@ const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri)
 let db;
 
-async function connectToMongo(){
+export async function connectToMongo(){
     if (!db){
         await client.connect();
         db = client.db('SMMS');
     }
 }
-
-export default connectToMongo();
